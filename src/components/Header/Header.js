0ecda5ccdb051b8647ade75750b30edead66cf2a
jubair-codes <you@example.com>
2025-03-30 20:44:45 +0600

    new

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import headerLogo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={headerLogo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">OrderReview</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
