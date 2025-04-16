7f25edf8d622f3da1a94bf22d747e882350f7445
jubair-codes <you@example.com>
2025-04-16 21:56:26 +0600

    updated router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Shop from './components/Shop/Shop';  
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Review/Review.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Review.css';
+
+const Review = () => {
+    return (
+        <div>
+            <h1>Reivew page</h1>
+        </div>
+    );
+};
+
+export default Review;
